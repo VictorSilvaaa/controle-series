e7c706c19f4552917335f50e4a8564d2e9d509af
João Victor da Silva Sales <56794256+VictorSilvaaa@users.noreply.github.com>
2024-01-19 17:10:59 -0300

    Initial commit

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index(Request $request)
+    {
+        $series = [
+            'Punisher',
+            'Lost',
+            'Grey\'s Anatomy'
+        ];
+
+        $html = '<ul>';
+        foreach ($series as $serie) {
+            $html .= "<li>$serie</li>";
+        }
+        $html .= '</ul>';
+
+        return $html;
+    }
+}
