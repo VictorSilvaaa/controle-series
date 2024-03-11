fa8049660c859d3ecb047ba290d9bb805231ed0c
João Victor da Silva Sales <56794256+VictorSilvaaa@users.noreply.github.com>
2024-03-11 09:30:44 -0300

    add links

--- app/Models/Series.php
+++ app/Models/Series.php
@@ -2,7 +2,8 @@
 
 namespace App\Models;
 
-use Attribute;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -15,6 +16,7 @@ class Series extends Model
     protected $casts = [
         'watched' => 'boolean'
     ];
+    protected $appends = ['links'];
 
     public function seasons()
     {
@@ -39,4 +41,24 @@ class Series extends Model
     //        get: fn($watched)=>(bool) $watched,
     //     );
     // }
+
+    public function links(): Attribute 
+    {
+        return new Attribute(
+            get: fn () => [
+                [
+                    'rel' => 'self',
+                    'url' => "/api/series/{$this->id}"
+                ],
+                [
+                    'rel' => 'seasons',
+                    'url' => "/api/series/{$this->id}/seasons"
+                ],
+                [
+                    'rel' => 'episodes',
+                    'url' => "/api/series/{$this->id}/episodes"
+                ],
+            ],
+        );
+    }
 }
